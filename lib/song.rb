7b9b9614e08f8b1220d35eeb02029e99fc28ba92
Martinique <me@martidolce.com>
2020-11-27 07:02:35 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+# song.rb
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist&.name
+  end
+end
